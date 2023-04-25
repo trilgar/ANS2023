5e1cb860bd32593d31539830c2a5dd7dd6128540
trilgar <zaritkiy.alexey@gmail.com>
2023-04-25 13:51:39 +0300

    lab6

--- lab3/src/main/java/com/example/lab3/UploadData.java
+++ lab3/src/main/java/com/example/lab3/UploadData.java
@@ -14,9 +14,14 @@ import org.springframework.data.elasticsearch.client.RestClients;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class UploadData {
-    public static final String FILE_PATH = "src/main/java/com/example/lab3/anandtech.json";
+    public static final String FILE_PATH = "src/main/java/com/example/lab3/combined.json";
     public static final String INDEX_NAME = "items";
     public static final ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("localhost:9200").build();
 
@@ -44,9 +49,22 @@ public class UploadData {
 
     private static InfoItem[] parseFromJson(String filePath, ObjectMapper objectMapper) {
         try {
-            return objectMapper.readValue(new File(filePath), InfoItem[].class);
+            InfoItem[] items = objectMapper.readValue(new File(filePath), InfoItem[].class);
+            for (InfoItem item : items) {
+                item.setPubDate(generateRandomDate());
+            }
+            return items;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String generateRandomDate() {
+        LocalDateTime start = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
+        LocalDateTime end = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
+        long startEpoch = start.toEpochSecond(ZoneOffset.UTC);
+        long endEpoch = end.toEpochSecond(ZoneOffset.UTC);
+        long randomEpoch = ThreadLocalRandom.current().nextLong(startEpoch, endEpoch);
+        return Instant.ofEpochSecond(randomEpoch).toString();
     }
 }
